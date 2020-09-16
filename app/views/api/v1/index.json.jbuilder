502a3c2e5b5267b35f3a678ddee308d3dca9d8c7
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-16 10:49:31 +0200

    home page in facts

--- app/views/api/v1/index.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.array! @plants do |plant|
-  json.extract! plant, :id, :name, :description
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      root to: 'plants#index', via: :options
+      resources :plants, only: [ :index ], via: :options
     end
   end
+
+  root to: 'pages#home'
 end
