4b4205b6b97a94dbf7a9fa174c5ddf6542921fe5
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-16 10:41:33 +0200

    deleted useless home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :plants, only: [ :index ], via: :options
+      root to: 'plants#index', via: :options
     end
   end
-
-  root to: 'pages#home'
 end
